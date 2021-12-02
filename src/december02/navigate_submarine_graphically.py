d2afac822c511d9a8990dfd144d60978e5250346
Alex Barten <mail@alexbarten.com>
2021-12-02 15:15:54 +0100

    Add graphical route of the submarine

---
+++ src/december02/navigate_submarine_graphically.py
@@ -0,0 +1,45 @@
+import turtle
+
+
+def main():
+    compute_submarine_position('course.txt')
+
+
+def compute_submarine_position(course):
+    horizontal_position = 0
+    depth = 0
+    submarine_.penup()
+    submarine_.setposition(-500,350)
+    submarine_.pendown()
+
+    with open(course, 'r') as navigation_units:
+        for navigation_unit in navigation_units:
+            direction, amount = navigation_unit.split()
+            amount = int(amount)
+            if direction == "forward":
+                horizontal_position += amount
+                submarine_.setheading(0)
+                submarine_.forward(amount)
+            elif direction == "down":
+                depth += amount
+                submarine_.setheading(270)
+                submarine_.forward(amount)
+            elif direction == "up":
+                depth -= amount
+                submarine_.setheading(90)
+                submarine_.forward(amount)
+            else:
+                print("You are trying me, aren't you?")
+
+    print("Final horizontal position = ", horizontal_position)
+    print("Final depth = ", depth)
+    print("Computed position = ", horizontal_position * depth)
+
+    turtle.done()
+
+
+submarine_ = turtle.Turtle()
+submarine_.screen.setworldcoordinates(-500, -1000, 1000, 500)
+
+if __name__ == "__main__":
+    main()
