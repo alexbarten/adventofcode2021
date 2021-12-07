f9d4f82255775a781486937353ae1ccc6de6c9f0
Alex Barten <mail@alexbarten.com>
2021-12-07 19:16:10 +0100

    Remove test-print statements.

--- src/december07/align_crabs_2_mean.py
+++ src/december07/align_crabs_2_mean.py
@@ -1,10 +1,9 @@
 def main():
-    crabs = crabs_to_list("src/december07/test.txt")
+    crabs = crabs_to_list("src/december07/crabs.txt")
     mean = determine_mean(crabs)
     steps = calculate_steps(crabs, mean)
-    # print(crabs)
-    print(mean)
-    print(steps)
+    print("The mean is:", mean)
+    print("The number of steps for all crabs is:", steps)
 
 
 def crabs_to_list(crabsfile):
@@ -18,8 +17,6 @@ def crabs_to_list(crabsfile):
 
 def determine_mean(crabs):
     mean = int(sum(crabs) / len(crabs))
-    a = (sum(crabs) / len(crabs))
-    print("non-int mean is ", a)
     if sum(crabs) % len(crabs) != 0:
         mean += 1
 
